836ec4c44e455466aa9bb979ccf663205abc92d1
nt-vinh <nvinh@seas.upenn.edu>
2024-01-22 01:42:18 -0500

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -13,7 +13,7 @@ def pin_to_ipfs(data):
     					"authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI2MjRjYTk1OS1jZDFkLTQ2NzEtOTY3My01MTEzODQ4ZTI2NTQiLCJlbWFpbCI6InZpbmhjaXRAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjY4NWVhOGIxNTc5MTg2MWU2YzM2Iiwic2NvcGVkS2V5U2VjcmV0IjoiNDRmMzE4YWI2ZDA3YWIyMmFjMWVjMDU2MTNiYjZiNDU3YjllOWY1ZmQ5MGQ0NWViMzIzY2Q2Y2IzMjlmZjYyMSIsImlhdCI6MTcwNTg5ODA1MX0.DEvs3wXWcQfzw3dpQxuidgup82IqyhAurLU3XntpfOk"
 						}
 	response = requests.post(url, json=payload, headers=headers)
-	return response['IpfsHash']
+	return response.json()['IpfsHash']
 
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
