07923c48021f5862522e93da3fec2d4c9a0c7bb8
nt-vinh <nvinh@seas.upenn.edu>
2024-02-12 05:39:42 -0500

    Add files via upload

--- salted_hashing.py
+++ salted_hashing.py
@@ -1,9 +1,13 @@
 import hashlib
 import os
+import string
+import random
 
 def myhash(m):
     #Generate random nonce
-    nonce = 'my_random_nonce'
+    # nonce = 'my_random_nonce'
+    nonce = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k=len(m)))
     #Generate hex digest
     h_hex = hashlib.sha256((nonce + m).encode('utf-16')).hexdigest()
     return nonce, h_hex
