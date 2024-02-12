1b78250686a78e183c935d253b6ef7569f56d1be
nt-vinh <nvinh@seas.upenn.edu>
2024-02-12 05:37:38 -0500

    Add files via upload

--- salted_hashing.py
+++ salted_hashing.py
@@ -5,7 +5,7 @@ def myhash(m):
     #Generate random nonce
     nonce = 'my_random_nonce'
     #Generate hex digest
-    h_hex = hashlib.sha256(nonce + m).hexdigest()
+    h_hex = hashlib.sha256((nonce + m).encode('utf-16')).hexdigest()
     return nonce, h_hex
 
 
